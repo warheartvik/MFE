8a29381c3195c381df54c63fdef08f2effe638be
Victor Marval <vmarval88@hotmail.com>
2021-06-07 11:35:34 -0500

    creación del yml de configuracion

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
@@ -18,10 +17,6 @@ const devConfig = {
                 marketing: 'marketing@http://localhost:9091/remoteEntry.js'
             },
             shared: packageJson.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-
         })
     ]
 }
