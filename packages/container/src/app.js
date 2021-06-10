5d5ce6aa72f5e79c28100c0190e5bf086ceb5eb8
Victor Marval <vmarval88@hotmail.com>
2021-06-09 23:15:35 -0500

    cambios de nuevo

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 export default () => {
     return <div>
-        <h1>Hola!!!!!!!!!!!!!!</h1>
+        <h1>Hola!</h1>
         <hr/>
         <MarketingApp/>
     </div>
