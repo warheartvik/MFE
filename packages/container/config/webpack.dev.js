27c629aeb79ca1186347b1c59a8575da0ca91cd3
Victor Marval <vmarval88@hotmail.com>
2021-06-09 22:42:52 -0500

    archivos originales

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,25 +1,24 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    historyApiFallback: {
-      index: 'index.html',
+    mode: 'development',
+    devServer: {
+        port: 9090,
+        historyApiFallback: {
+            index: 'index.html'
+        }
     },
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-      },
-      shared: packageJson.dependencies,
-    }),
-  ],
-};
+    plugins: [
+        new ModuleFederationPlugin({
+            remotes: {
+                marketing: 'marketing@http://localhost:9091/remoteEntry.js'
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, devConfig)
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './app'
 
-// Mount function to start up the app
+//mount function para iniciar el app
+
 const mount = (el) => {
-  ReactDOM.render(<App />, el);
-};
-
-// If we are in development and in isolation,
-// call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
-
-  if (devRoot) {
-    mount(devRoot);
-  }
+    ReactDom.render(
+        <App/>,
+        el
+    )
 }
 
-// We are running through container
-// and we should export the mount function
-export { mount };
+//si estamos en modo desarrollo iniciar mount de una
+
+if(process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector('#marketing-dev-root')
+    if(devRoot){
+        mount(devRoot)
+    }
+}
+
+//asumimos que estamos iniciando desde container
+//entonces exportamos mount
+
+export {
+    mount
+}
