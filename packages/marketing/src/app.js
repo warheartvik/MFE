66747c47fafaa4994b77520a2ad868329072b810
Victor Marval <vmarval88@hotmail.com>
2021-06-10 20:47:41 -0500

    reparando class collision

--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
@@ -8,7 +12,7 @@ import Pricing from './components/Pricing'
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing}></Route>
